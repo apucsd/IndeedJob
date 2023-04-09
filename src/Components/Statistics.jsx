4f9856e9be582d4ec0f3a0bdb79d7bb053bcb860
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-04-09 23:48:03 +0600

    added statistics chart

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Area,
+} from "recharts";
 
 const Statistics = () => {
-  return <div>only statistics</div>;
+  const assignments = useLoaderData();
+  //   console.log(assignments);
+  return (
+    <div className="w-[80%] h-[300px] mx-auto">
+      <ResponsiveContainer>
+        <AreaChart
+          data={assignments}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <XAxis dataKey="assignmentNumber" />
+          <YAxis dataKey="mark" />
+          <Area dataKey="mark" stroke="#8884d8" fill="#8884d8" />
+          <Tooltip dataKey="mark" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
       {
         path: "statistics",
         element: <Statistics></Statistics>,
+        loader: () => fetch("assignments.json"),
       },
       {
         path: "applied_jobs",
