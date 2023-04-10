93c3d94d277901217e2ee46bc26c64866a77d5f8
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-04-10 20:51:11 +0600

    added blogs questions

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Blogs = () => {
-  return <div>blog is here</div>;
+  return (
+    <div className="md:w-1/2 mx-auto my-8">
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 my-1\3"
+      >
+        <div className="collapse-title text-xl font-medium">
+          1. When should you use context API?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 my-1\3"
+      >
+        <div className="collapse-title text-xl font-medium">
+          2 .What is a custom hook?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 my-1\3"
+      >
+        <div className="collapse-title text-xl font-medium">
+          3. What is useRef?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="collapse collapse-arrow border border-base-300 bg-base-100 my-1\3"
+      >
+        <div className="collapse-title text-xl font-medium">
+          4. What is useMemo?
+        </div>
+        <div className="collapse-content">
+          <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
